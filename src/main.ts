4bbdafcfa88427938f561a3f83210a6cf4f7cd4a
rmitchell999 <rmitchell0402@gmail.com>
2025-01-23 12:18:27 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports'; // Ensure this path is correct
+import './assets/main.css'; // Import your global styles
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
 
 Amplify.configure(awsconfig);
 
