e28b5bc495d490c70a9adde5946928a917e4b082
rmitchell999 <rmitchell0402@gmail.com>
2025-01-23 10:27:36 +0000

    Update Frontend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,35 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyoncall": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifyoncall361d2ced": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Trigger446736ca": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "s3storage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
-<script setup lang="ts">
-import Todos from './components/Todos.vue'
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Authenticator } from '@aws-amplify/ui-vue';
+import OnCallApplication from './components/OnCallApplication.vue';
+export default defineComponent({
+  components: {
+    Authenticator,
+    OnCallApplication
+  }
+});
 </script>
-
 <template>
   <main>
-    <Todos />
+    <Authenticator>
+      <template #default="{ signOut, user }">
+        <OnCallApplication :signOut="signOut" />
+      </template>
+    </Authenticator>
   </main>
 </template>
-
+<style>
+@import '@aws-amplify/ui-vue/styles.css';
+</style>
