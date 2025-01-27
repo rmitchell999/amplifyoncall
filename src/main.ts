9f83f7f710ed23080b2c733ab1d33b7cd8f0ac36
rmitchell999 <rmitchell0402@gmail.com>
2025-01-27 12:19:20 +0000

    Update

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './assets/main.css'; // Import your global styles
 import { Amplify } from 'aws-amplify';
-import outputs from "@/amplify_outputs.json";
+import awsconfig from './aws-exports';
 
 
-Amplify.configure(outputs);
+Amplify.configure(awsconfig);
 
 createApp(App).mount('#app');
